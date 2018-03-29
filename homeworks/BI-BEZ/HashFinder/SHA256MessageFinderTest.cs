644061897cacceb2862f856222b2099784532f51
Tomas Izo <tomas@izo.sk>
2018-03-29 15:37:30 +0200

    Hash finder part done

---
+++ homeworks/BI-BEZ/HashFinder/SHA256MessageFinderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using NUnit.Framework;
+
+namespace HashFinder
+{
+    [TestFixture]
+    public class SHA256MessageFinderTest
+    {
+        
+        [TestCase("AA")]
+        [TestCase("AA","BB")]
+        [TestCase("AA","BB", "CC")]
+        public void Test(params string[] pattern)
+        {
+            var result = SHA256MessageFinder.LookForMessageWithHashPattern(null, pattern);
+
+            // check if result was found
+            Assert.NotNull(result);
+            
+            // check if pattern matches its SHA-256 hash
+            var sha256 = SHA256.Create();
+            var hash = sha256.ComputeHash(result);              
+            Assert.True(!pattern.Where((element, index) => Convert.ToByte(element, 16) != hash[index]).Any());
+        }
+    }
+}
